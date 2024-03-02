92b3d423581ca0f759a5c34d425627dc10402e11
Annettuu <122024704+Annettuu@users.noreply.github.com>
2024-03-02 18:13:53 +0700

    Add: themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import "./index.scss"
+import "./styles/index.scss"
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Suspense} from "react";
+import {Suspense, useContext, useState} from "react";
+import {UseTheme} from "./theme/useTheme";
 
 export const App = () => {
+    const  {theme, toggleTheme} = UseTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme} >TOOGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
