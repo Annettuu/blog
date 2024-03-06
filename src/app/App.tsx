ec6e72dd8e096750d77b246af6157434b6334dea
Annettuu <122024704+Annettuu@users.noreply.github.com>
2024-03-06 17:42:23 +0700

    Add: i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,20 @@ import {classNames} from "shared/lib/classNames";
 import {AppRouter} from "app/provides/router";
 import {NavBar} from "widgets/NavBar";
 import {Sidebar} from "widgets/Sidebar";
+import {Suspense} from "react";
 
 export const App = () => {
     const  {theme} = UseTheme();
 
     return (
         <div className={classNames('app',{},[theme])}>
-            <NavBar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback=''>
+                <NavBar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     )
 }
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './NavBar.module.scss'
 import {classNames} from "shared/lib/classNames";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {useTranslation} from "react-i18next";
 
 interface NavBarProps {
     className?: string;
 }
-
 export const NavBar = ({className}: NavBarProps) => {
     return (
         <div className={classNames(classes.navbar, {}, [className])}>
