82e19aa50606c9628dc4a8b4bc3370ae9bfd0f0c
Annettuu <122024704+Annettuu@users.noreply.github.com>
2024-03-08 03:31:54 +0700

    Add: BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,8 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
 
 export function buildPlugins({paths, isDev}: BuildOptions) :webpack.WebpackPluginInstance[] {
     return [
@@ -15,6 +17,9 @@ export function buildPlugins({paths, isDev}: BuildOptions) :webpack.WebpackPlugi
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ]
 }
